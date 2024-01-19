10e4f2f1bdbc95998c18e383d5531e1a5366d255
Ahmet Mahir Demirelli <122171884+Ahmet-MahirDEMIRELLI@users.noreply.github.com>
2024-01-19 19:27:35 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
 	public static void main(String[] args){
-		System.out.print("YTU Database Management Semester Project\n2023-2024 Fall Semester\nStudents involved in the project:\n	Ahmet Mahir DEMİRELLİ\n	Yavuz ÇETİN\n	Osman Berkay SUKAS\n	Ahmed Asım SEVİMLİ");
+		System.out.print(  "YTU Database Management Semester Project\n"
+				 + "2023-2024 Fall Semester\nStudents involved in the project:\n	"
+				 + "Ahmet Mahir DEMİRELLİ\n	"
+				 + "Yavuz ÇETİN\n	"
+				 + "Osman Berkay SUKAS\n	"
+				 + "Ahmed Asım SEVİMLİ");
 	}
 }
